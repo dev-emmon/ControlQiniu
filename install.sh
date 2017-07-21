65da8168086e92032ee8996d629a6d57208c6533
Em <liz3002@126.com>
2017-07-21 16:06:35 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ sudo make install
 cd ~/ControlQiniu/
 wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
 tar zxvf yasm-1.3.0.tar.gz
-cd yasm
+cd yasm-1.3.0
 ./configure
 make
 sudo make install
